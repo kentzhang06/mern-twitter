6c4c8ea6fc78125df08ba2bfa2bb26cd2eba457f
Kent Zhang <kentzhang31@yahoo.com>
2021-10-10 19:40:21 -0700

    restart project

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://kentzhang31:RHYpJQ6rGRXY97CL@cluster0.4u9gf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: 'o$=rLnxA#N<GhL%'
+            //Make sure this is your own unique string
+}
+
+// "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjNhMDlhMjBjYWViYTI5YmFjNWJmZCIsImhhbmRsZSI6InRlc3QiLCJlbWFpbCI6InRlc3RAbWFpbC5jb20iLCJpYXQiOjE2MzM5MTkxMzAsImV4cCI6MTYzMzkyMjczMH0.JRnrWo1QD4muOPvV6fCzzxeTxCFCBq6AUAXit178oIw"
