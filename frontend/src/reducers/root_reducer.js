db21dc6e90abe7b3f99b8b783b3afc8f495e647e
Kent Zhang <kentzhang31@yahoo.com>
2021-10-10 20:35:30 -0700

    updating frontend

---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import sessionReducer from './session_reducer';
+import errorsReducer from './errors_reducer';
+import tweetsReducer from './tweet_reducer'
+
+const rootReducer = combineReducers({
+    session: sessionReducer,
+    tweets: tweetsReducer,
+    errors: errorsReducer
+})
+
+export default rootReducer;
